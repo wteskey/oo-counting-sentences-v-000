70b82b7ad9c9a2b4845add4968550d4e602d5f35
William J Teskey <willjteskey@gmail.com>
2018-07-31 20:15:58 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -5,7 +5,7 @@ class String
   def sentence?
     self.end_with?(".")
   end
-
+  
   def question?
     self.end_with?("?")
   end
@@ -14,8 +14,12 @@ class String
     self.end_with?("!")
   end
 
-  def count_sentences
-    split = self.split(/[.!?]\s/)
-    split.count
+# split into an array without included (., or ?) and then count the sentences. 
+
+  def count_sentences  
+  self.split(/[.?!]/)    
+  .delete_if |s|    
+  s.size < 2.count  
   end
+
 end
